51d0322c65e3df4508ac2e3fcb6b719d3c509b3c
Selina <selina@kionnali.com>
2024-11-13 16:15:40 +0000

    react bootstrap installed and NavBar component added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './App.css';
+import NavBar from './Components/NavBar';
 
 export default function App() {
   return(
     <>
+      <NavBar />
       <p>Hello World!</p>
     </>
   )
